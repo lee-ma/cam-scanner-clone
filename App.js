ba2c88771284b5d69f0b80f8de302afbad144a5e
lee-ma <leema9321@gmail.com>
2019-06-19 14:29:58 -0700

    add camera to project

--- App.js
+++ App.js
@@ -1,19 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasCameraPermission: null,
+      type: Camera.Constants.Type.back
+    }
+  }
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted'
+    });
+  }
+  
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={styles.container }>
+          <Camera style={styles.camera} type={this.state.type}>
+          </Camera>
+          <View>
+            <TouchableOpacity
+              onPress={() => {
+                this.setState({
+                  type:
+                    this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                });
+              }}>
+                <Text style={{ fontSize: 18, marginBottom: 10 }}> Flip </Text>
+              </TouchableOpacity>
+            </View>
+        </View>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  camera: {
+    width:Dimensions.get('window').width,
+    height: Dimensions.get('window').width * 4 / 3,
+  }
 });
+
+export default App
