dd6e11bbb3dd076ae01f731973451c0ce35f87ce
lee-ma <leema9321@gmail.com>
2019-06-19 15:14:04 -0700

    style camerae button

--- components/CaptureButton.js
+++ components/CaptureButton.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default CaptureButton = ({ snap }) => (
-  <View>
+  <View style={styles.buttonContainer}>
     <TouchableOpacity
+      style={styles.cameraIcon}
       onPress={snap}>
-      <Text style={{ fontSize: 18, marginBottom: 10 }}> Take Picture </Text>
+      <Text style={styles.text}><MaterialCommunityIcons name="circle-slice-8" size={64} color="white"/></Text>
     </TouchableOpacity>
   </View>
 );
+
+const styles = StyleSheet.create({
+  cameraIcon: {
+    display: "flex",
+    height: "100%",
+  },
+  text: {
+    height: "auto",
+    display: "flex",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    display: "flex",
+    height: "100%",
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: "#000"
+  }
+})
